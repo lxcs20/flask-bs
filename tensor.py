4e6fec56f0e0bae95e0c492dc83d922a8e0ace0d
Lanthong <98867063+lxcs20@users.noreply.github.com>
2024-05-08 22:45:30 +0700

    init tensor

---
+++ tensor.py
@@ -0,0 +1,7 @@
+from keras import models
+from keras._tf_keras.keras.preprocessing.sequence import pad_sequences
+import keras._tf_keras.keras as tfk
+import numpy as np
+# from tensorflow.python.keras.utils import 
+from laonlp.tokenize import word_tokenize
+import pandas as pd
